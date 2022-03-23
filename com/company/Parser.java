20c38b43dcd71056c763ebd89e3e2e603e0ea412
khoohatt <85897592+khoohatt@users.noreply.github.com>
2022-03-23 21:20:37 +0300

    Update Parser.java

--- com/company/Parser.java
+++ com/company/Parser.java
@@ -5,44 +5,25 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.io.*;
-import java.util.Dictionary;
-import java.util.Enumeration;
-import java.util.HashMap;
 
 public class Parser {
-    public static void parse() {//main(String[] args) {
-        Connection connect = findPage();
+    public static void parse() {
+        String lang = "et";
+        Connection connect = findPage(lang);
         try {
             Document document = connect.get();
-            /* go(document);
-            Elements elements1 = document.select("#page-wrapper > div.wrapper.wrapper-content.animated.article.article-page > div > div:nth-child(1) > div > div > div.text-center.article-title");
-            Elements elements2 = document.select("#page-wrapper > div.wrapper.wrapper-content.animated.article.article-page > div > div:nth-child(1) > div > div > div.blockquote > div:nth-child(1) > span");
-            System.out.println(elements1.get(0).text());
-            System.out.println(elements2.get(0).text());
-    }
-
-    public static void go(Document document) {
-        Elements o = document.getElementsByTag("#all_words > div > div:nth-child(1) > ul > li:nth-child(1)");
-        o = o.getElementsByTag("li").get(0);
-            var o1 = document.getElementsByTag("ul").get(0);
-            System.out.println(document.select("#all_words > div > div:nth-child(1) > ul > li:nth-child(" + let + ") > a").text()); //o.first().getElementsByTag("a"));
-            do {
-                o = o.nextSibling();
-                if (o.tagName == "LI") break;
-            } while (true);
-        } */
             String word = findWord(document);
-            System.out.println(word);
             if (word.charAt(word.length() - 1) == '-') {
                 word = word.replaceAll("-", "");
             }
             System.out.println(word);
             word = word.replaceAll(" ", "_");
-            System.out.println(word);
-            connect = Jsoup.connect("https://gufo.me/dict/etru/" + word).userAgent("Mozilla");
-            word = connect.get().select("#dictionary-acticle > article > h1").text();
+            StringBuilder sb = new StringBuilder();
+            sb.append("https://gufo.me/dict/").append(lang).append("ru/").append(word);
+            //connect = Jsoup.connect("https://gufo.me/dict/" + lang + "ru/" + word).userAgent("Mozilla");
+            connect = Jsoup.connect(String.valueOf(sb)).userAgent("Mozilla");
+
             String trans = connect.get().select("#dictionary-acticle > article > p > span").text();
-            System.out.println(word);
             System.out.println(trans);
             //Dictionary dictionary = new Dictionary();
             //HashMap map = new HashMap();
@@ -51,7 +32,7 @@ public class Parser {
         }
     }
 
-    public static Connection findPage() {
+    public static Connection findPage(String lang) {
         /* URL u = new URL ( "https://gufo.me/dict/etru?page=234&letter=%C5%A1");
         HttpURLConnection huc = ( HttpURLConnection )  u.openConnection();
         huc.setRequestMethod ("HEAD");
@@ -61,32 +42,50 @@ public class Parser {
         char[] letters = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 'š', 't', 'u', 'v', 'w', 'õ', 'ä', 'ö', 'ü'};
         char let = letters[(int) ((Math.random()*((26 - 1) + 1)) + 1)];
         int count = 0;
-        while (true) {
-            Connection connect = Jsoup.connect("https://gufo.me/dict/etru?page=" + (count + 1) + "&letter=" + let).userAgent("Mozilla");
+
+
+        StringBuilder sb = new StringBuilder();
+        while (true) {  // TODO придумать что-нибудь для оптимизации чтения страницы (f.e. читать заголовок, но без использования get(), однако для этого нужно обходить защиту сайта (ошибку 403) - я еще не разобралась, как)
+            sb.append("https://gufo.me/dict/").append(lang).append("ru?page=").append(count + 1).append("&letter=").append(let);
+            //Connection connect = Jsoup.connect("https://gufo.me/dict/" + lang + "ru?page=" + (count + 1) + "&letter=" + let).userAgent("Mozilla");
+            Connection connect = Jsoup.connect(String.valueOf(sb)).userAgent("Mozilla");
+            sb.setLength(0);
             try {
-                connect.get().select("head > title").text(); //!= ("Not Found (#404)")) {
+                //connect.get().select("head > title").text(); //!= ("Not Found (#404)")) {  Not Found (#404)
+                if (connect.get().title().equals("Not Found (#404)")) {
+                    break;
+                }
                 count++;
-                //}
             } catch (IOException e) {
                 break;
             }
         }
+
         System.out.println(count);
         count = (int) ((Math.random()*((count - 1) + 1)) + 1);
-        return Jsoup.connect("https://gufo.me/dict/etru?page=" + count + "&letter=" + let).userAgent("Mozilla");
+        sb.append("https://gufo.me/dict/etru?page=").append(count).append("&letter=").append(let);
+        //return Jsoup.connect("https://gufo.me/dict/etru?page=" + count + "&letter=" + let).userAgent("Mozilla");
+        return Jsoup.connect(String.valueOf(sb)).userAgent("Mozilla");
     }
 
     public static String findWord(Document document) {
         int count = 0;
+        StringBuilder sb = new StringBuilder();
         while (true) {
-            if (document.select("#all_words > div > div:nth-child(1) > ul > li:nth-child(" + (count + 1) + ") > a").text() == "") {
+            sb.append("#all_words > div > div:nth-child(1) > ul > li:nth-child(").append(count + 1).append(") > a");
+            //if (document.select("#all_words > div > div:nth-child(1) > ul > li:nth-child(" + (count + 1) + ") > a").text() == "") {
+            if (document.select(String.valueOf(sb)).text().equals("")) {
+                sb.setLength(0);
                 break;
             } else {
+                sb.setLength(0);
                 count++;
             }
         }
         System.out.println(count);
         int let = ((int) ((Math.random()*((count - 1) + 1)) + 1));
-        return (document.select("#all_words > div > div:nth-child(1) > ul > li:nth-child(" + let + ") > a").text());
+        sb.append("#all_words > div > div:nth-child(1) > ul > li:nth-child(").append(let).append(") > a");
+        //return (document.select("#all_words > div > div:nth-child(1) > ul > li:nth-child(" + let + ") > a").text());
+        return (document.select(String.valueOf(sb)).text());
     }
 }
