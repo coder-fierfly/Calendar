f3d77b36b24d865ace70e62f104fb1f8481c4d7c
khoohatt <85897592+khoohatt@users.noreply.github.com>
2022-04-07 17:08:22 +0300

    Add files via upload

---
+++ com/company/PartyParser.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.*;
+
+public class PartyParser {
+    public String parse(String id) {
+        Connection connect = findPage(id);
+        try {
+            Document document = connect.get();
+            String word = findWord(document);
+            System.out.println(word);
+            return word;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    private Connection findPage(String id) {
+        System.out.println(id);
+        String[] str = id.split("/");
+        id = String.join("-", str[2], str[0], str[1]);
+        StringBuilder sb = new StringBuilder();
+        sb.append("https://www.calend.ru/day/").append(id);
+        return Jsoup.connect(String.valueOf(sb)).userAgent("Mozilla");
+    }
+
+    private String findWord(Document document) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("body > div.wrapper > div.block.main > div.index_page.content > div:nth-child(1) > ul > li:nth-child(1) > div.caption > span > a");
+        return (document.select(String.valueOf(sb)).text());
+    }
+}
+
