fe22a8f9d7d883d5a90769510d97004e38561d24
khoohatt <patch_soul@mail.ru>
2022-04-25 15:28:15 +0300

    исправлены ошибки с файлами, отображение словаря, добавлена возможность сохранять новые праздники.

--- com/company/ChangeLang.java
+++ com/company/ChangeLang.java
@@ -4,7 +4,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.FileWriter;
@@ -18,26 +17,21 @@ public class ChangeLang {
     public ToggleGroup answers;
 
     //кнопки выбора языка
-    //TODO по какой-то причине RadioButton выбирается не только 1 а можно выбрать все. возможно это по тому, что я пока что не подключила функцию
     public void radioLangButton() {
         if (de.isSelected()) {
-            //Logic.setLang("de");
             addLang("de");
-            //save.setOnAction(e -> saveLang());
             System.out.println("de");
+            saveLang();
         } else if (en.isSelected()) {
-            //Logic.setLang("en");
             addLang("en");
-            //save.setOnAction(e -> saveLang());
-            System.out.println("en");
+            System.out.println("de");
+            saveLang();
         } else if (et.isSelected()) {
-            //Logic.setLang("et");
             addLang("et");
-            //save.setOnAction(e -> saveLang());
             System.out.println("et");
-            //TODO здесь должно вылезать сообщение если язык не выбран, но чел пытается что-то сохранить
+            saveLang();
         } else {
-            save.setOnAction(e -> makeChoice.setText("Вы не сделали выбор."));
+            makeChoice.setText("Вы не сделали выбор.");
         }
     }
 
@@ -52,11 +46,9 @@ public class ChangeLang {
         }
     }
 
-    public AnchorPane stageLang;
-    //кнопочка сохранения языка. в идеале должна закрывать окно.
+    //кнопочка сохранения языка. закрывает окно.
     public void saveLang() {
-        //TODO сделать в парсере смену языка
-        save.setOnAction(e -> stageLang.getScene().getWindow());
-//        stageLang.close();
+        Stage stage = (Stage) save.getScene().getWindow();
+        stage.close();
     }
 }
